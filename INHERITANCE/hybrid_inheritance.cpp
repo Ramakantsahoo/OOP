77eb36afd85a8c40cf6d4e702f0e071721c7d89c
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-05 23:53:48 +0530

    Update hybrid_inheritance.cpp

--- INHERITANCE/hybrid_inheritance.cpp
+++ INHERITANCE/hybrid_inheritance.cpp
@@ -1,54 +1,63 @@
-// Define a class student having attributes name and roll no..Derive a class test having mark of two subjects.Define a class sport having attributes to store sports 
-// mark.Derive a class result from test and sport to calculate total marks and average.
-#include<bits/stdc++.h>
+// Define a class student having attributes name and roll no..Derive a class test having mark of two subjects.Define a class sport having attributes to store sports mark.Derive a class result from test and sport to calculate total marks and average.
+#include <bits/stdc++.h>
 using namespace std;
-class student{
-public:    
+class student
+{
+public:
     char name[30];
     int roll;
-    void input(){
-        cout<<"Name: ";
-        cin>>name;
-        cout<<"Roll no.: ";
-        cin>>roll;
+    void input()
+    {
+        cout << "Name: ";
+        cin >> name;
+        cout << "Roll no.: ";
+        cin >> roll;
     }
 };
-class Test : public student{
+class Test : public student
+{
 public:
-    float m1,m2;
-    void input(){
-        cout<<"Enter marks of two subjects: ";
-        cin>>m1>>m2;
+    float m1, m2;
+    void input()
+    {
+        cout << "Enter marks of two subjects: ";
+        cin >> m1 >> m2;
     }
 };
-class Sports{
+class Sports
+{
 public:
     float sports_mark;
-    void input(){
-        cout<<"Enter sports mark: ";
-        cin>>sports_mark;
-    }    
+    void input()
+    {
+        cout << "Enter sports mark: ";
+        cin >> sports_mark;
+    }
 };
-class result : public Test,public Sports{
+class result : public Test, public Sports
+{
 public:
     float avg;
     float total;
-    void clc(){
-        avg=(m1+m2+sports_mark)/3;
-        total=m1+m2+sports_mark;
+    void clc()
+    {
+        avg = (m1 + m2 + sports_mark) / 3;
+        total = m1 + m2 + sports_mark;
     }
-    void display(){
+    void display()
+    {
         clc();
-        cout<<"The marks of two subjects and sports marks are "<<m1<<" "<<m2<<" "<<sports_mark<<" respectivly"<<endl;
-        cout<<"The total mark is: "<<total<<endl;
-        cout<<"The average mark is: "<<avg<<endl;
+        cout << "The marks of two subjects and sports marks are " << m1 << " " << m2 << " " << sports_mark << " respectivly" << endl;
+        cout << "The total mark is: " << total << endl;
+        cout << "The average mark is: " << avg << endl;
     }
 };
-int main() {
+int main()
+{
     result r;
-    r.student :: input();
-    r.Test :: input();
-    r.Sports :: input();
+    r.student ::input();
+    r.Test ::input();
+    r.Sports ::input();
     r.display();
     return 0;
 }
