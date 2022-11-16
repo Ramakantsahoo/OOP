b27dfbb01263b609dda3197da72e520b1d8a9618
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-17 00:38:00 +0530

    Add files via upload

---
+++ BASICS/forwhiledowhileloops.cpp
@@ -0,0 +1,44 @@
+// For while loops and setwidth.
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // for(int i=1;i<=4;i++){
+    //     cout<<i<<endl;
+    // }
+    // int i=0;
+    // while( i <= 4){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // int i=0;
+    // do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }while(i<0);
+    long double a = 0.234;
+    float b = 0;
+    int c = 9;
+    cout << setw(4) << a << endl;
+    cout << setw(4) << b << endl;
+    cout << setw(4) << c << endl;
+
+    return 0;
+}
+// Terminal 1
+// 1
+// 2
+// 3
+// 4
+// Terminal 2
+// 0
+// 1
+// 2
+// 3
+// 4
+// Terminal 3
+// 0
+// Terminal 4
+// 0.234
+//    0
+//    9
