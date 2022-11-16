0adc8a54aee1abc937ff808da0fa8a9f57ab71c9
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-17 00:36:56 +0530

    Add files via upload

---
+++ BASICS/enum.cpp
@@ -0,0 +1,22 @@
+// Enum data type
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    enum meal
+    {
+        breakfast,
+        lunch,
+        dinner
+    };
+    meal m1 = breakfast;
+    cout << m1 << endl;
+    cout << breakfast << endl;
+    cout << lunch << endl;
+    cout << dinner << endl;
+}
+// Terminal
+// 0
+// 0
+// 1
+// 2
