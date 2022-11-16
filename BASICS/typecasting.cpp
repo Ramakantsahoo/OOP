a900e431bc9655f5f6aef8007b289f14ccecc303
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-11-17 00:39:59 +0530

    Add files via upload

---
+++ BASICS/typecasting.cpp
@@ -0,0 +1,15 @@
+// Typecasting
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long double a = 19.548;
+    int b = 10;
+    long double sum = a + b;
+    cout << sum << endl;
+    cout << int(sum) << endl;
+    return 0;
+}
+// Terminal
+// 29.548
+// 29
