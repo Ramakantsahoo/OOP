0c15ee86c20e7f72e12a40eb2f054bf09d9c08f4
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-12-09 12:45:42 +0530

    Update class1.cpp

--- CLASS/class1.cpp
+++ CLASS/class1.cpp
@@ -1,4 +1,7 @@
 // Class 
+// A class in C++ is the building block that leads to Object-Oriented programming. It is a user-defined data type, which holds its own data members and member functions,
+// which can be accessed and used by creating an instance of that class. A C++ class is like a blueprint for an object.
+
 #include <bits/stdc++.h>
 using namespace std;
 // :: is scope resolution operator
