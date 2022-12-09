a12d45fb57b0bcbea2e37afeae94fabf50360d3f
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-12-09 12:27:16 +0530

    Update enum.cpp

--- BASICS/enum.cpp
+++ BASICS/enum.cpp
@@ -1,4 +1,7 @@
 // Enum data type
+// Enumeration or Enum in C is a special kind of data type defined by the user. It consists of constant integrals or integers that are given names by a user.
+// The use of enum in C to name the integer values makes the entire program easy to learn, understand, and maintain by the same or even different programmer.
+
 #include <bits/stdc++.h>
 using namespace std;
 int main()
