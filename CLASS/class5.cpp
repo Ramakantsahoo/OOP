0945759e8e58be2079b8930fd76d3a2ad167bc13
Ramakant <98227695+Ramakantsahoo@users.noreply.github.com>
2022-12-15 10:30:33 +0530

    Update class5.cpp

--- CLASS/class5.cpp
+++ CLASS/class5.cpp
@@ -1,31 +1,42 @@
 // Complex sum
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class complexx{
-    int a,b;
-    public:
-        void setdata(int v1,int v2){
-            a=v1;
-            b=v2;
-        }
-        
-        void sumofcomplex(complexx z1,complexx z2){
-            complexx z3;
-            z3.setdata((z1.a+z2.a),(z1.b+z2.b));
-            
-        }
-        void display(){
-            cout<<a<<"+"<<b<<"i"<<endl;
-        }
+class complexx
+{
+    int a, b;
+
+public:
+    void setdata(int v1, int v2)
+    {
+        a = v1;
+        b = v2;
+    }
+
+    complexx sumofcomplex(complexx z1, complexx z2)
+    {
+        complexx z3;
+        z3.a = z1.a + z2.a;
+        z3.b = z1.b + z2.b;
+        return z3;
+    }
+    void display()
+    {
+        cout << "The sum is: " << a << " + " << b << "i" << endl;
+    }
 };
 
-int main() {
-    complexx c1,c2,c3;
-    c1.setdata(2,3);
+int main()
+{
+    complexx c1, c2, c3, c4;
+    c1.setdata(2, 3);
     c1.display();
-    c2.setdata(3,3);
+    c2.setdata(3, 3);
     c2.display();
-    c3.sumofcomplex(c1,c2);
-    c3.display();
+    c4 = c3.sumofcomplex(c1, c2);
+    c4.display();
     return 0;
 }
+// Terminal
+// The sum is: 2 + 3i
+// The sum is: 3 + 3i
+// The sum is: 5 + 6i
